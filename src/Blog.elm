e1ba665426635e4bee2f084056736e3253adc4fb
Antti Halla <antti.halla@gmail.com>
2016-03-11 15:06:05 +0200

    Index page intro, styles

--- src/Blog.elm
+++ src/Blog.elm
@@ -1,6 +1,7 @@
 module Blog (init, view, update) where
 
-import Html exposing (text)
+import Html exposing (text, div, p)
+import Html.Attributes exposing (class)
 import Effects exposing (Effects)
 import Signal
 
@@ -14,7 +15,25 @@ init =
 
 view : Signal.Address Action -> Model -> Html.Html
 view action model =
-    text "Hello World"
+    div [class "container"] [frontPage]
+
+
+frontPage =
+    div [class "front-page"] [div [class "row"]  [col1, col2]]
+
+
+col1 =
+    div [class "col-md-8"] [intro]
+
+col2 =
+    div [class "col-md-4"] [text "col2"]
+
+intro =
+    div [class "intro"] [
+        p [] [text "The web is my platform."],
+        p [] [text "I've been experimenting with a range of technologies to create software for the web. Besides helping me think and learn &ndash; and paying my bills &ndash; it enabled me to say goodbye to the cubicle and explore the world."]
+    ]
+
 
 --  UPDATE --
 
