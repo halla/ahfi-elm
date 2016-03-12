7bd7202a9f7a3ffe3031f00ee2a80ce4f2e38405
Antti Halla <antti.halla@gmail.com>
2016-03-12 08:16:01 +0200

    Front page parametrizing

--- src/Blog.elm
+++ src/Blog.elm
@@ -1,29 +1,35 @@
 module Blog (init, view, update) where
 
-import Html exposing (text, div, p)
+import Html exposing (text, div, p, ul, li, a)
 import Html.Attributes exposing (class)
 import Effects exposing (Effects)
 import Signal
 
-type alias Model =
+
+type alias Post =
     { title: String }
+
+type alias Model =
+    List Post
+
+posts =
+    [{ title = "hep"}]
 
 init : (Model, Effects Action)
 init =
-    ({ title = "Hep"
-    }, Effects.none)
+    (posts, Effects.none)
 
 view : Signal.Address Action -> Model -> Html.Html
 view action model =
-    div [class "container"] [frontPage]
+    div [class "container"] [frontPage model]
 
 
-frontPage =
-    div [class "front-page"] [div [class "row"]  [col1, col2]]
+frontPage model =
+    div [class "front-page"] [div [class "row"]  [col1 model, col2]]
 
 
-col1 =
-    div [class "col-md-8"] [intro]
+col1 model =
+    div [class "col-md-8"] [intro, postsList model]
 
 col2 =
     div [class "col-md-4"] [text "col2"]
@@ -34,6 +40,15 @@ intro =
         p [] [text "I've been experimenting with a range of technologies to create software for the web. Besides helping me think and learn &ndash; and paying my bills &ndash; it enabled me to say goodbye to the cubicle and explore the world."]
     ]
 
+
+postLink post =
+    a [] [(text post.title)]
+
+renderPost post =
+    li [] [postLink post]
+
+postsList model =
+    ul [class "blog-list-simple list-unstyled"] (List.map renderPost model)
 
 --  UPDATE --
 
